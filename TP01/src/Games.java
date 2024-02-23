381039766029a6f5e4b1c821b5b4e636561a298d
Catarina F. M. Castro <catfmcastro@gmail.com>
2024-02-23 16:50:25 -0300

    mexendo no Games.java

--- TP01/src/Games.java
+++ TP01/src/Games.java
@@ -1,4 +1,4 @@
-package src;
+package scr;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -9,7 +9,8 @@ import java.io.IOException;
 public class Games {
     private int id, steamID, release_date;
     private float price;
-    private String name, short_description, genres, publishers;
+    private String name, short_description, genres, publishers, supports_linux;
+    private boolean grave; // Lápide
 
     public Games() {
         this.id = -1;
@@ -20,17 +21,20 @@ public class Games {
         this.genres = "N";
         this.publishers = "N";
         this.release_date = -1;
+        this.supports_linux = "N";
+        this.grave = false;
     }
 
-    public Games(int id, int steamID, float price, String name, String short_description, String genres,
-            String publishers, int release_date) {
+    public Games(boolean grave, int id, int steamID, float price, String name, String short_description, String genres, String publishers, String supports_linux, int release_date) {
         this.id = id;
+        this.grave = grave;
         this.steamID = steamID;
         this.price = price;
         this.name = name;
         this.short_description = short_description;
         this.genres = genres;
         this.publishers = publishers;
+        this.supports_linux = supports_linux;
         this.release_date = release_date;
     }
 
@@ -48,23 +52,28 @@ public class Games {
         return day + "/" + month + "/" + year;
     }
 
-    public void printGame() {
+    public void printScren() {
         System.out.println("Id: " + this.id + " - " + "SteamID: " + this.steamID + " - " + "Preço: R$" + this.price
                 + "\n" + "Nome: " + this.name + "\n" + "Descrição: " + this.short_description + "\n" + "Gêneros: "
-                + this.genres + "\n" + "Publisher: " + this.publishers + "\n" + "Data de Lançamento: "
-                + hoursToDate(this.release_date) + "\n");
+                + this.genres + "\n" + "Publicador: " + this.publishers + "\n" + "Data de Lançamento: "
+                + hoursToDate(this.release_date) + "\n" + "Grave: " + this.grave + "\n" + "Suporte Linux: " + this.supports_linux + "\n");
     }
 
-    public byte[] createbyteArray() throws IOException {
+    // Transforma dados do game em vetor de bytes e insere no arq.
+    public byte[] createByteArray () throws IOException{
+        // grave,id,steamID,name,price,short_descritiption,genres,publishers,supports_linux,release_date
+
         ByteArrayOutputStream by = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(by);
         dos.writeInt(id);
+        dos.writeBoolean(grave);
         dos.writeInt(steamID);
         dos.writeUTF(name);
         dos.writeDouble(price);
         dos.writeUTF(short_description);
         dos.writeUTF(genres);
         dos.writeUTF(publishers);
+        dos.writeUTF(supports_linux);
         dos.writeInt(release_date);
         dos.close();
 
@@ -74,6 +83,7 @@ public class Games {
     public void readBytes(byte by[]) throws IOException {
         ByteArrayInputStream vet = new ByteArrayInputStream(by);
         DataInputStream dos = new DataInputStream(vet);
+        this.grave = dos.readBoolean();
         this.id = dos.readInt();
         this.steamID = dos.readInt();
         this.name = dos.readUTF();
@@ -81,6 +91,7 @@ public class Games {
         this.short_description = dos.readUTF();
         this.genres = dos.readUTF();
         this.publishers = dos.readUTF();
+        this.supports_linux = dos.readUTF();
         this.release_date = dos.readInt();
         dos.close();
     }
